02135e4367a653a7685641e00963e3f17751f85e
igorivnaolga <109050006+igorivnaolga@users.noreply.github.com>
2022-08-17 13:52:01 +1200

    updated

--- src/index.js
+++ src/index.js
@@ -18,31 +18,45 @@ console.log(currentDate);
 let date = document.querySelector("#current-date");
 date.innerHTML = currentDate;
 
-function search(event) {
+function displayWeatherCondition(response) {
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+}
+
+function search(city) {
+  let apiKey = "ff833e479a6ba8688180b478fbf4489b";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+
+function handleSubmit(event) {
   event.preventDefault();
-  let city = document.querySelector("#city");
-  let cityInput = document.querySelector("#city-input");
-  city.innerHTML = cityInput.value;
+  let city = document.querySelector("#city-input").value;
+  search(city);
 }
 
 let searchForm = document.querySelector("#search-button");
-//
+searchForm.addEventListener("click", handleSubmit);
 
-searchForm.addEventListener("click", search);
+function searchLocation(position) {
+  let apiKey = "ff833e479a6ba8688180b478fbf4489b";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
 
-function convertToFahrenheit(event) {
-  event.preventDefault();
-  let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = 66;
+  axios.get(apiUrl).then(displayWeatherCondition);
 }
 
-function convertToCelsius(event) {
+function getCurrentLocation(event) {
   event.preventDefault();
-  let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = 19;
+  navigator.geolocation.getCurrentPosition(searchLocation);
 }
-let fahrenheitLink = document.querySelector("#fahrenheit-link");
-fahrenheitLink.addEventListener("click", convertToFahrenheit);
+let currentLocationButton = document.querySelector("#current-button");
+currentLocationButton.addEventListener("click", getCurrentLocation);
 
-let celsiusLink = document.querySelector("#celsius-link");
-celsiusLink.addEventListener("click", convertToCelsius);
+search("Christchurch");
